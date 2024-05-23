2551c8fcbffea13bfbd32b8843d36deb6ee6ee78
Matthieu Flce <flacelierematthieu@gmail.com>
2024-05-23 11:15:18 +0200

    Scenario 2

--- SpecFlowProject/Features/Scrutin.feature
+++ SpecFlowProject/Features/Scrutin.feature
@@ -2,7 +2,7 @@
 
     @scrutin
     Scenario: Obtenir le vainqueur du vote
-        Given 2 votes pour A et 3 votes pour B
+        Given 1 votes pour A et 3 votes pour B
         When le scrutin est terminé
         Then le vainqueur devrait être B
 
@@ -11,3 +11,9 @@
         Given 2 votes pour A et 3 votes pour B
         When le scrutin est en cours
         Then le vainqueur ne peut pas être déterminé
+        
+    @scrutin
+    Scenario: Si un candidat obtient > 50% des voix, il est déclaré vainqueur du scrutin
+        Given 1 votes pour A et 3 votes pour B
+        When le scrutin est terminé
+        Then le vainqueur devrait être B
--- SpecFlowProject/Features/Scrutin.feature.cs
+++ SpecFlowProject/Features/Scrutin.feature.cs
@@ -111,7 +111,7 @@ namespace SpecFlowProject.Features
             {
                 this.ScenarioStart();
 #line 5
-        testRunner.Given("2 votes pour A et 3 votes pour B", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Given ");
+        testRunner.Given("1 votes pour A et 3 votes pour B", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Given ");
 #line hidden
 #line 6
         testRunner.When("le scrutin est terminé", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "When ");
@@ -161,6 +161,49 @@ namespace SpecFlowProject.Features
 #line hidden
 #line 13
         testRunner.Then("le vainqueur ne peut pas être déterminé", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
+#line hidden
+            }
+            this.ScenarioCleanup();
+        }
+        
+        [Xunit.SkippableFactAttribute(DisplayName="Si un candidat obtient > 50% des voix, il est déclaré vainqueur du scrutin")]
+        [Xunit.TraitAttribute("FeatureTitle", "Scrutin")]
+        [Xunit.TraitAttribute("Description", "Si un candidat obtient > 50% des voix, il est déclaré vainqueur du scrutin")]
+        [Xunit.TraitAttribute("Category", "scrutin")]
+        public virtual void SiUnCandidatObtient50DesVoixIlEstDeclareVainqueurDuScrutin()
+        {
+            string[] tagsOfScenario = new string[] {
+                    "scrutin"};
+            System.Collections.Specialized.OrderedDictionary argumentsOfScenario = new System.Collections.Specialized.OrderedDictionary();
+            TechTalk.SpecFlow.ScenarioInfo scenarioInfo = new TechTalk.SpecFlow.ScenarioInfo("Si un candidat obtient > 50% des voix, il est déclaré vainqueur du scrutin", null, tagsOfScenario, argumentsOfScenario, this._featureTags);
+#line 16
+    this.ScenarioInitialize(scenarioInfo);
+#line hidden
+            bool isScenarioIgnored = default(bool);
+            bool isFeatureIgnored = default(bool);
+            if ((tagsOfScenario != null))
+            {
+                isScenarioIgnored = tagsOfScenario.Where(__entry => __entry != null).Where(__entry => String.Equals(__entry, "ignore", StringComparison.CurrentCultureIgnoreCase)).Any();
+            }
+            if ((this._featureTags != null))
+            {
+                isFeatureIgnored = this._featureTags.Where(__entry => __entry != null).Where(__entry => String.Equals(__entry, "ignore", StringComparison.CurrentCultureIgnoreCase)).Any();
+            }
+            if ((isScenarioIgnored || isFeatureIgnored))
+            {
+                testRunner.SkipScenario();
+            }
+            else
+            {
+                this.ScenarioStart();
+#line 17
+        testRunner.Given("1 votes pour A et 3 votes pour B", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Given ");
+#line hidden
+#line 18
+        testRunner.When("le scrutin est terminé", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "When ");
+#line hidden
+#line 19
+        testRunner.Then("le vainqueur devrait être B", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
 #line hidden
             }
             this.ScenarioCleanup();
